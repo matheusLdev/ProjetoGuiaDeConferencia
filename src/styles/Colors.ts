2dac458e4e8bbc4c21401bb065cd99735918eb43
matheusldev <mgdesenvolvedor@gmail.com>
2024-09-05 22:21:28 -0300

    feat: adicionando cores e fonts que serão usados no projeto.

---
+++ src/styles/Colors.ts
@@ -0,0 +1,10 @@
+const Colors = {
+  yellow: '#F7C40F',
+  black: '#181200',
+  light_yellow: '#FCDD6D',
+  off_white: '#FEF7DF',
+  light_gray: '#F3F6F8',
+  red: '#CB0000',
+};
+
+export default Colors;
