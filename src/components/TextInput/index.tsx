f39c5a10f245eda694a9d1da7fc103692649d7bc
matheusldev <mgdesenvolvedor@gmail.com>
2024-09-07 18:53:32 -0300

    refactor: ajustando nome do styles

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import { View, Text, TextInput, TextInputProps } from 'react-native';
-import { styles } from './style';
+import {View, Text, TextInput, TextInputProps} from 'react-native';
+import {styles} from './styles';
 
 interface InputTextProps extends TextInputProps {
   label: string;
   id: string;
 }
 
-export const InputText: React.FC<InputTextProps> = ({ label, id, ...rest }) => {
+export const InputText: React.FC<InputTextProps> = ({label, id, ...rest}) => {
   return (
     <View style={styles.container}>
       <Text style={styles.label}>{label}</Text>
       <TextInput
-        {...rest}
         placeholder={label}
         style={styles.input}
         testID={id}
+        {...rest}
       />
     </View>
   );
