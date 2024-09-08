1a06b2fad7ba58c7b0069795ee7d82419e917e65
matheusldev <mgdesenvolvedor@gmail.com>
2024-09-08 20:32:36 -0300

    feat: adicionando tela de histórico de conferência

--- src/screens/HistoryConferenceGuide/index.tsx
+++ src/screens/HistoryConferenceGuide/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Header from '../../components/Header';
-import {ContainerMain} from './styles';
+import {Container, ContainerMain, SubTitleText} from './styles';
+import Button from '../../components/Button';
 
 export const HistoryConferenceGuide = () => {
   return (
     <ContainerMain>
       <Header ButtonReturn={true} TitleHeader="Histórico de Conferência" />
+      <Container>
+        <SubTitleText>
+          Solange Machado, seu histórico de conferência está vazio.
+          {/* Solange Machado, aqui está seu histórico de conferência: */}
+        </SubTitleText>
+        <Button text="VOLTAR PARA PÁGINA INICIAL" onPress={() => {}} />
+      </Container>
     </ContainerMain>
   );
 };
--- src/screens/HistoryConferenceGuide/styles.ts
+++ src/screens/HistoryConferenceGuide/styles.ts
@@ -5,3 +5,20 @@ export const ContainerMain = styled.View`
   background-color: ${Colors.yellow};
   flex: 1;
 `;
+
+export const Container = styled.View`
+  background-color: ${Colors.light_gray};
+  flex: 1;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  elevation: 5;
+  padding: 20px;
+  justify-content: space-between;
+`;
+
+export const SubTitleText = styled.Text`
+  color: ${Colors.black};
+  font-size: 18px;
+  font-weight: 600;
+  padding: 20px;
+`;
