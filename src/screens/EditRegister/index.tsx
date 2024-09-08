f8e3fcd1980eb59f134798c5baa39dc774bf0d27
matheusldev <mgdesenvolvedor@gmail.com>
2024-09-08 20:32:21 -0300

    feat: adicionando tela de edição de cadastro

--- src/screens/EditRegister/index.tsx
+++ src/screens/EditRegister/index.tsx
@@ -1,11 +1,41 @@
 import React from 'react';
-import {ContainerMain} from './styles';
+import {Container, ContainerBtn, ContainerInfo, ContainerMain} from './styles';
+import EditIcon from '../../assets/images/edit.png';
 import Header from '../../components/Header';
+import Button from '../../components/Button';
+import Colors from '../../styles/Colors';
+import {InputText} from '../../components/InputText';
 
 export const EditRegister = () => {
   return (
     <ContainerMain>
       <Header ButtonReturn={true} TitleHeader="Editar Cadastro" />
+      <Container>
+        <ContainerInfo>
+          <InputText
+            id="firstName"
+            label="Nome"
+            placeholder="Digite seu nome"
+            icon={EditIcon}
+          />
+          <InputText
+            id="lastName"
+            label="Sobrenome"
+            placeholder="Digite seu sobrenome"
+            icon={EditIcon}
+          />
+          <InputText
+            id="ticketPrice"
+            label="Valor da Passagem"
+            placeholder="Digite o valor da passagem"
+            icon={EditIcon}
+          />
+        </ContainerInfo>
+        <ContainerBtn>
+          <Button text="VOLTAR" onPress={() => {}} color={Colors.off_white} />
+          <Button text="SALVAR" onPress={() => {}} />
+        </ContainerBtn>
+      </Container>
     </ContainerMain>
   );
 };
--- src/screens/EditRegister/styles.ts
+++ src/screens/EditRegister/styles.ts
@@ -5,3 +5,24 @@ export const ContainerMain = styled.View`
   background-color: ${Colors.yellow};
   flex: 1;
 `;
+
+export const Container = styled.View`
+  background-color: ${Colors.light_gray};
+  flex: 1;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  elevation: 5;
+  padding: 20px;
+  justify-content: space-between;
+`;
+
+export const ContainerInfo = styled.View`
+  top: 5%;
+`;
+
+export const ContainerBtn = styled.View`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
