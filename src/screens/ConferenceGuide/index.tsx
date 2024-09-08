9a07e66996823959ba113eb90c87228f3b948f03
matheusldev <mgdesenvolvedor@gmail.com>
2024-09-08 19:28:31 -0300

    feat: add tela para registrar nova coferência

--- src/screens/ConferenceGuide/index.tsx
+++ src/screens/ConferenceGuide/index.tsx
@@ -1,11 +1,110 @@
-import React from 'react';
-import {ContainerMain} from './styles';
+import React, {useState} from 'react';
+import {Container, ContainerMain} from './styles';
 import Header from '../../components/Header';
+import {NavigationProp, useNavigation} from '@react-navigation/native';
+import {RootStackParamList} from '../../routes/types';
+import {ScrollView} from 'react-native';
+import {InputText} from '../../components/InputText';
+import Button from '../../components/Button';
+import Colors from '../../styles/Colors';
 
 export const ConferenceGuide = () => {
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+  const [form, setForm] = useState({
+    initialRoulette: '',
+    finalRoulette: '',
+    totalPassenger: '',
+    gratuity: '',
+    transportVoucher: '',
+    noCard: '',
+    normalPassenger: '',
+    ticketPrice: '',
+    cashInHand: '',
+  });
+
+  const handleChange = (value: string, field: keyof typeof form) => {
+    setForm({...form, [field]: value});
+  };
+
+  const handleSubmit = () => {
+    console.log('Registrei nova conferência');
+  };
+
   return (
     <ContainerMain>
       <Header ButtonReturn={true} TitleHeader="Registrar Nova Conferência" />
+      <ScrollView>
+        <Container>
+          <InputText
+            label="Roleta Inicial - RI"
+            id="initialRoulette"
+            placeholder="Digite Roleta Inicial"
+            value={form.initialRoulette}
+            onChangeText={text => handleChange(text, 'initialRoulette')}
+          />
+          <InputText
+            label="Roleta Final - RF"
+            id="finalRoulette"
+            placeholder="Digite Roleta Final"
+            value={form.finalRoulette}
+            onChangeText={text => handleChange(text, 'finalRoulette')}
+          />
+          <InputText
+            label="Total Passageiro - PT"
+            id="totalPassenger"
+            placeholder="Total de passageiro"
+            value={form.totalPassenger}
+            onChangeText={text => handleChange(text, 'totalPassenger')}
+          />
+          <InputText
+            label="Gratuidade - GR"
+            id="gratuity"
+            placeholder="Digite as gratuidades"
+            value={form.gratuity}
+            onChangeText={text => handleChange(text, 'gratuity')}
+          />
+          <InputText
+            label="Vale Transporte - VT"
+            id="transportVoucher"
+            placeholder="Digite a quantidade de vale-transporte"
+            value={form.transportVoucher}
+            onChangeText={text => handleChange(text, 'transportVoucher')}
+          />
+          <InputText
+            label="Sem Cartão - SC"
+            id="noCard"
+            placeholder="Digite a quantidade passageiro sem cartão"
+            value={form.noCard}
+            onChangeText={text => handleChange(text, 'noCard')}
+          />
+          <InputText
+            label="Passageiro Normal - P"
+            id="normalPassenger"
+            placeholder="Total de passageiro normais"
+            value={form.normalPassenger}
+            onChangeText={text => handleChange(text, 'normalPassenger')}
+          />
+          <InputText
+            label="Valor da Passagem"
+            id="ticketPrice"
+            placeholder="Valor da passagem"
+            value={form.ticketPrice}
+            onChangeText={text => handleChange(text, 'ticketPrice')}
+          />
+          <InputText
+            label="Dinheiro em Caixa"
+            id="cashInHand"
+            placeholder="Dinheiro em caixa"
+            value={form.cashInHand}
+            onChangeText={text => handleChange(text, 'cashInHand')}
+          />
+          <Button
+            color={Colors.yellow}
+            text="EFETUAR CONFERÊNCIA"
+            onPress={handleSubmit}
+          />
+        </Container>
+      </ScrollView>
     </ContainerMain>
   );
 };
--- src/screens/ConferenceGuide/styles.ts
+++ src/screens/ConferenceGuide/styles.ts
@@ -5,3 +5,13 @@ export const ContainerMain = styled.View`
   background-color: ${Colors.yellow};
   flex: 1;
 `;
+
+export const Container = styled.View`
+  background-color: ${Colors.light_gray};
+  flex: 1;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  elevation: 5;
+  padding: 20px;
+  justify-content: space-between;
+`;
