ff527fd390490526cb013e29f881cfe47743b7ea
matheusldev <mgdesenvolvedor@gmail.com>
2024-09-08 17:34:27 -0300

    feat: add função de retorno ao botão do header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import Arrow from '../../assets/images/arrow.png';
 import {HeaderProps} from '../../types';
 import {HeaderContainer, HeaderTitle, ImgStyled} from './styles';
+import {useNavigation} from '@react-navigation/native';
+import {TouchableOpacity} from 'react-native';
 
 const Header = ({ButtonReturn, TitleHeader}: HeaderProps) => {
+  const navigation = useNavigation();
+
   return (
     <HeaderContainer>
-      {ButtonReturn && <ImgStyled source={Arrow} />}
+      {ButtonReturn && (
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <ImgStyled source={Arrow} />
+        </TouchableOpacity>
+      )}
       <HeaderTitle hasImage={ButtonReturn}>{TitleHeader}</HeaderTitle>
     </HeaderContainer>
   );
